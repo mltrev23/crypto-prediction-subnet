5d764f6be22e7dd61e1a01e345af3dff32dd41c6
Rene.Bustamante <rene.bustamante@chain-insights.ai>
2024-08-24 06:31:36 -0500

    feat: created challenge synapse

--- template/protocol.py
+++ template/protocol.py
@@ -19,58 +19,15 @@
 
 import typing
 import bittensor as bt
+from pydantic import Field
+from typing import List
 
-# TODO(developer): Rewrite with your protocol definition.
-
-# This is the protocol for the dummy miner and validator.
-# It is a simple request-response protocol where the validator sends a request
-# to the miner, and the miner responds with a dummy response.
-
-# ---- miner ----
-# Example usage:
-#   def dummy( synapse: Dummy ) -> Dummy:
-#       synapse.dummy_output = synapse.dummy_input + 1
-#       return synapse
-#   axon = bt.axon().attach( dummy ).serve(netuid=...).start()
-
-# ---- validator ---
-# Example usage:
-#   dendrite = bt.dendrite()
-#   dummy_output = dendrite.query( Dummy( dummy_input = 1 ) )
-#   assert dummy_output == 2
-
-
-class Dummy(bt.Synapse):
-    """
-    A simple dummy protocol representation which uses bt.Synapse as its base.
-    This protocol helps in handling dummy request and response communication between
-    the miner and the validator.
-
-    Attributes:
-    - dummy_input: An integer value representing the input request sent by the validator.
-    - dummy_output: An optional integer value which, when filled, represents the response from the miner.
-    """
-
+class Challenge(bt.Synapse):
     # Required request input, filled by sending dendrite caller.
-    dummy_input: int
+    timestamp: str = Field(title = 'Timestamp', description = 'The time stamp when prediction requires')
 
     # Optional request output, filled by receiving axon.
-    dummy_output: typing.Optional[int] = None
+    output: typing.Optional[List[float]] = Field(default = None, title = 'Prediction Result', description = 'Next 6 predicted values')
 
-    def deserialize(self) -> int:
-        """
-        Deserialize the dummy output. This method retrieves the response from
-        the miner in the form of dummy_output, deserializes it and returns it
-        as the output of the dendrite.query() call.
-
-        Returns:
-        - int: The deserialized response, which in this case is the value of dummy_output.
-
-        Example:
-        Assuming a Dummy instance has a dummy_output value of 5:
-        >>> dummy_instance = Dummy(dummy_input=4)
-        >>> dummy_instance.dummy_output = 5
-        >>> dummy_instance.deserialize()
-        5
-        """
-        return self.dummy_output
+    def deserialize(self) -> List[float]:
+        return self.output
