9b2ae143d89b11948ac15c6ff8f476a6062ce848
mltrev23 <trevor.dev23@gmail.com>
2024-08-24 10:34:45 -0500

    fix: validator

--- neurons/validator.py
+++ neurons/validator.py
@@ -17,55 +17,51 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+
 import time
+
+# Bittensor
 import bittensor as bt
 
-from template.protocol import Challenge
-from template.validator.reward import get_rewards
-from template.utils.uids import get_random_uids
+# import base validator class which takes care of most of the boilerplate
+from template.base.validator import BaseValidatorNeuron
+# Bittensor Validator Template:
+from template.validator import forward
 
-from pytz import timezone
-import datetime
 
-async def forward(self):
+class Validator(BaseValidatorNeuron):
     """
-    The forward function is called by the validator every time step.
+    Your validator neuron class. You should use this class to define your validator's behavior. In particular, you should replace the forward function with your own logic.
 
-    It is responsible for querying the network and scoring the responses.
+    This class inherits from the BaseValidatorNeuron class, which in turn inherits from BaseNeuron. The BaseNeuron class takes care of routine tasks such as setting up wallet, subtensor, metagraph, logging directory, parsing config, etc. You can override any of the methods in BaseNeuron if you need to customize the behavior.
 
-    Args:
-        self (:obj:`bittensor.neuron.Neuron`): The neuron object which contains all the necessary state for the validator.
-
+    This class provides reasonable default behavior for a validator such as keeping a moving average of the scores of the miners and using them to set weights at the end of each epoch. Additionally, the scores are reset for new hotkeys at the end of each epoch.
     """
-    # TODO(developer): Define how the validator selects a miner to query, how often, etc.
-    # get_random_uids is an example method, but you can replace it with your own.
-    miner_uids = get_random_uids(self, k=self.config.neuron.sample_size)
-    
-    ny_timezone = timezone('America/New_York')
-    current_time_ny = datetime.now(ny_timezone)
-    timestamp = current_time_ny.isoformat()
-    
-    synapse = Challenge(timestamp = timestamp)
 
-    # The dendrite client queries the network.
-    responses = await self.dendrite(
-        # Send the query to selected miner axons in the network.
-        axons=[self.metagraph.axons[uid] for uid in miner_uids],
-        # Construct a dummy query. This simply contains a single integer.
-        synapse=synapse,
-        # All responses have the deserialize function called on them before returning.
-        # You are encouraged to define your own deserialization function.
-        deserialize=True,
-    )
+    def __init__(self, config=None):
+        super(Validator, self).__init__(config=config)
 
-    # Log the results for monitoring purposes.
-    bt.logging.info(f"Received responses: {responses}")
+        bt.logging.info("load_state()")
+        self.load_state()
 
-    # TODO(developer): Define how the validator scores responses.
-    # Adjust the scores based on responses from miners.
-    rewards = get_rewards(self, query=self.step, responses=responses)
+        # TODO(developer): Anything specific to your use case you can do here
 
-    bt.logging.info(f"Scored responses: {rewards}")
-    # Update the scores based on the rewards. You may want to define your own update_scores function for custom behavior.
-    self.update_scores(rewards, miner_uids)
-    time.sleep(5)
+    async def forward(self):
+        """
+        Validator forward pass. Consists of:
+        - Generating the query
+        - Querying the miners
+        - Getting the responses
+        - Rewarding the miners
+        - Updating the scores
+        """
+        # TODO(developer): Rewrite this function based on your protocol definition.
+        return await forward(self)
+
+
+# The main function parses the configuration and runs the validator.
+if __name__ == "__main__":
+    with Validator() as validator:
+        while True:
+            bt.logging.info(f"Validator running... {time.time()}")
+            time.sleep(5)
