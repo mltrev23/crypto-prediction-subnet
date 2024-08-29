d87200044cb7eec7c5892328ecbac6ca32df128f
mltrev23 <trevor.dev23@gmail.com>
2024-08-28 20:03:17 -0500

    feat: base_lstm model trained and saved

--- base_model/build_lstm_model.py
+++ base_model/build_lstm_model.py
@@ -7,6 +7,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 import os
 import joblib
+from base_miner.get_tao_price import get_data, scale_data
+import pandas as pd
 
 def create_base_lstm(scaler:MinMaxScaler, X_scaled:np.ndarray, y_scaled:np.ndarray) -> float:
     model_name = "models/base_lstm"
@@ -78,4 +80,21 @@ def create_base_regression(scaler:MinMaxScaler, X_scaled:np.ndarray, y_scaled:np
     print(f'Mean Squared Error: {mse}')
     
     return mse
+if __name__ == '__main__':
+    data = get_data()
+    scaler, X_data, y_data = scale_data(data)
+    
+    data['Datetime'] = pd.to_datetime(data['Datetime'])
 
+    # The timestamp sent by the validator need not be associated with an exact 5m interval
+    # It's on the miners to ensure that the time is rounded down to the last completed 5 min candle
+    # data.to_csv('mining_models/base_miner_data.csv')
+    input = data[['Open', 'High', 'Low', 'Volume', 'SMA_50', 'SMA_200', 'RSI', 'CCI', 'Momentum']]
+    type = 'lstm'
+
+    if(type != 'regression'):
+        input = np.array(input, dtype = np.float32).reshape(1, -1)
+        input = np.reshape(input, (-1, 1, input.shape[1]))
+        print(input)
+
+    if(type == 'lstm'): create_base_lstm(scaler = scaler, X_scaled = X_data, y_scaled = y_data)
